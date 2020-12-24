0e51be0ef7f0d84c84012664cd0415fb004ee32b
juned-shaikh <juned.shaikh.7415@gmail.com>
2020-12-24 11:21:59 +0530

    latest back to top

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2762,4 +2762,34 @@ var pro=s_no;
       }
     }
 
+
+    @HostListener('window:scroll')
+    checkScroll() {
+        
+      // window의 scroll top
+      // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+  
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  
+      console.log('[scroll]', scrollPosition);
+      
+      if (scrollPosition >= this.topPosToStartShowing) {
+        this.isShow = true;
+      } else {
+        this.isShow = false;
+      }
+    }
+  
+    // TODO: Cross browsing
+    gotoTop() {
+      window.scroll({ 
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth' ,
+        
+      });
+    }
+
+
+
 }
